3307ef48fee75901bbe8b6ef122febd509357d49
Gexon <alimgan@gmail.com>
2017-01-28 02:56:27 +0500

    Поппытка внедрения, начало, Behaviour Tree AI

--- src/monster/components.rs
+++ src/monster/components.rs
@@ -46,3 +46,44 @@ pub struct _MonsterMaps {
 }
 
 impl Component for _MonsterMaps {}
+
+
+/// Сосотояние монстра, для Behaviour Tree
+pub struct BehaviourGlobalState {
+    //    Всего 2 глобальных состояния:
+    //    1.  Размеренное. Монстр спокоен.
+    //    2.  Тревожное. Потеря здоровья.
+    pub state: u32,
+}
+
+impl Component for BehaviourGlobalState {}
+
+/// Сосотояние монстра, для Behaviour Tree
+pub struct BehaviourState {
+    //    Всего 6 состояний:
+    //    1.  Сон. Монстр ждет, в этот момент с ним ничего не происходит.
+    //    2.  Бодрствование. Случайное перемещение по полигону.
+    //    3.  Поиск пищи.
+    //    4.  Поиск воды.
+    //    5.  Прием пищи.
+    //    6.  Прием воды.
+    pub state: u32,
+}
+
+impl Component for BehaviourState {}
+
+/// Событий происходящее с монстром, для Behaviour Tree
+pub struct BehaviourEvent {
+    //    Всего 6 событий:
+    //    1.  Обнаружена еда.
+    //    2.  Обнаружена вода.
+    //    3.  Наступил голод.
+    //    4.  Наступила жажда.
+    //    5.  Выспался.
+    //    6.  Нет событий.
+    //    7.  Монстр насытился.
+    //    8.  Монстр напился.
+    pub event: u32,
+}
+
+impl Component for BehaviourEvent {}
